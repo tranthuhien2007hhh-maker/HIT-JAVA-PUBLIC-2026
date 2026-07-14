9f090f3c4201ba553134249877a4eac592e37992
Trần Thu Hiền <tranthuhien2007@gamil.com>
2026-07-14 16:01:35 +0700

    nhap

---
+++ Nhap/src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int age = 19;
+        double gpa = 3.05;
+        char grade = 'A';
+        boolean pass = true;
+        String name = "Hien";
+        System.out.println(age);
+        System.out.println(gpa);
+        System.out.println(grade);
+        System.out.println(pass);
+        System.out.println(name);
+    }
+}
