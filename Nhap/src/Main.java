460f809cd12064ca8e8f82fdbcf736a5934e263a
Trần Thu Hiền <tranthuhien2007@gamil.com>
2026-07-21 15:27:59 +0700

    ksjdrh

--- Nhap/src/Main.java
+++ Nhap/src/Main.java
@@ -2,15 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int age = 19;
-        double gpa = 3.05;
-        char grade = 'A';
-        boolean pass = true;
-        String name = "Hien";
-        System.out.println(age);
-        System.out.println(gpa);
-        System.out.println(grade);
-        System.out.println(pass);
-        System.out.println(name);
+        Student s = new Student("SV01", "An", 8.5);
+        System.out.println(s.getRank()); // Giỏi
     }
 }
